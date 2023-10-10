3c30f50c59ec264d46604240cbec66eb54244493
Carlos Otávio de Souza <carlosotavio_souza@apple.com>
2023-10-10 18:21:32 -0300

    adjusting tests

--- cypress/e2e/barrigaMock/accountMock.cy.js
+++ cypress/e2e/barrigaMock/accountMock.cy.js
@@ -18,9 +18,6 @@ describe("Account", function () {
     cy.clearLocalStorage();
   });
 
-  before(function () {
-    cy.getJwtToken(user.email, user.password);
-  });
   beforeEach(function () {
     // rota de saldos
     cy.intercept(
@@ -47,7 +44,8 @@ describe("Account", function () {
     cy.visit("/");
     loginPage.doLogin(user.email, user.password);
   });
-  it("should create an account successfully", function () {
+  it.only("should create an account successfully", function () {
+    // Lista de contas mockadas
     cy.intercept(
       {
         method: "GET",
@@ -57,73 +55,68 @@ describe("Account", function () {
         statusCode: 200,
         body: [
           {
-            id: 1940220,
-            nome: "newAccountUpdate",
+            id: 1,
+            nome: "Carteira",
             visivel: true,
-            usuario_id: 41550,
+            usuario_id: 1,
           },
           {
-            id: 1938470,
-            nome: "newAccountTestUI",
+            id: 2,
+            nome: "Banco",
             visivel: true,
-            usuario_id: 41550,
-          },
-          {
-            id: 1938471,
-            nome: "AccUpdated",
-            visivel: true,
-            usuario_id: 41550,
-          },
-          {
-            id: 1938472,
-            nome: "sameAccountName",
-            visivel: true,
-            usuario_id: 41550,
-          },
-          {
-            id: 1938464,
-            nome: "Conta para alterar",
-            visivel: true,
-            usuario_id: 41550,
-          },
-          {
-            id: 1938465,
-            nome: "Conta mesmo nome",
-            visivel: true,
-            usuario_id: 41550,
-          },
-          {
-            id: 1938466,
-            nome: "Conta para movimentacoes",
-            visivel: true,
-            usuario_id: 41550,
-          },
-          {
-            id: 1938467,
-            nome: "Conta com movimentacao",
-            visivel: true,
-            usuario_id: 41550,
-          },
-          {
-            id: 1938468,
-            nome: "Conta para saldo",
-            visivel: true,
-            usuario_id: 41550,
-          },
-          {
-            id: 1938469,
-            nome: "Conta para extrato",
-            visivel: true,
-            usuario_id: 41550,
+            usuario_id: 1,
           },
         ],
       }
-    ).as("saldo");
-    cy.visit("/");
-    loginPage.doLogin(user.email, user.password);
+    ).as("contasLista");
+    // Cadastro de conta mockado
+    cy.intercept(
+      {
+        method: "POST",
+        url: "https://barrigarest.wcaquino.me/contas",
+      },
+      {
+        body: {
+          id: 3,
+          nome: "newAccountMock",
+          visivel: true,
+          usuario_id: 1,
+        },
+      }
+    ).as("contasNova");
 
-    const newAccount = "newAccountTestUI";
+    const newAccount = "newAccountMock";
     header.navToAccounts();
+
+    cy.intercept(
+      {
+        method: "GET",
+        url: "https://barrigarest.wcaquino.me/contas",
+      },
+      {
+        statusCode: 200,
+        body: [
+          {
+            id: 1,
+            nome: "Carteira",
+            visivel: true,
+            usuario_id: 1,
+          },
+          {
+            id: 2,
+            nome: "Banco",
+            visivel: true,
+            usuario_id: 1,
+          },
+          {
+            id: 3,
+            nome: "newAccountMock",
+            visivel: true,
+            usuario_id: 1,
+          },
+        ],
+      }
+    ).as("contasSalvas");
     accountPage.addAccount(newAccount);
     accountPage.accountNameShouldBeVisible(newAccount);
 
